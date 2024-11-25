2d68606254a59465b49e917c3fbfd76343c5872a
SOUNAK GHOSH <sounak2309@gmail.com>
2024-11-25 16:19:09 +0530

    updated

--- JS-Projects/02_weather/script.js
+++ JS-Projects/02_weather/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cityNameDisplay = document.getElementById("city-name");
     const temperatureDisplay = document.getElementById("temperature");
     const descriptionDisplay = document.getElementById("description");
-    const errorMessage = document.getElementById("error-messag");
+    const errorMessage = document.getElementById("error-message");
 
     const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"; //env variables
 
@@ -29,18 +29,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
         const response = await fetch(url);
-        console.log(typeof response);
-        console.log("RESPONSE", response);
+        // console.log(typeof response);
+        // console.log("RESPONSE", response);
 
         if (!response.ok) {
-            throw new Error(" City Not found");
+            throw new Error("City not found");
         }
         const data = await response.json();
         return data;
     }
-
     function displayWeatherData(data) {
-        console.log(data);
+        // console.log(data);
         const { name, main, weather } = data;
         cityNameDisplay.textContent = name;
         temperatureDisplay.textContent = `Temperature : ${main.temp}`;
