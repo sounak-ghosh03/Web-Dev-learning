69dfdcff51c3c1ace2b0db782555f360be00f11d
SOUNAK GHOSH <sounak2309@gmail.com>
2025-05-21 17:06:46 +0530

    error handling and utils discussion

--- 05_BACKEND/complete backend/src/index.js
+++ 05_BACKEND/complete backend/src/index.js
@@ -7,18 +7,21 @@ dotenv.config({
     path: "./env",
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB CONNECTION FAILED !!!", err);
+    });
 
 
+    
 
 
-
-
-
-
-
-
-// ALTERNATIVE APPROACH
+// ALTERNATIVE APPROACH TO CONNECT DATABASE BY IIFEs
 /*
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
