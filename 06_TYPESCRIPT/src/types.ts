fdc13d23b29dabd19d4128c738915aa8be4ef475
SOUNAK GHOSH <sounak2309@gmail.com>
2025-12-06 22:02:03 +0530

    types and annotation

---
+++ 06_TYPESCRIPT/src/types.ts
@@ -0,0 +1,11 @@
+let drink = "chai"
+
+let cups = Math.random()>0.5?10:5;
+
+let channelName = "Tech"
+// channelName=121212/
+
+let chaiFlavour = "ginger tea";
+chaiFlavour= "lemon tea"
+
+let chaiOrder: boolean
